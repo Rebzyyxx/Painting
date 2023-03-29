48310670e76f0952ef22737fd3cba8b77d9bc580
Rebzyyxx <120789616+Rebzyyxx@users.noreply.github.com>
2023-03-29 13:52:50 +0500

    Initial commit

---
+++ app/src/main/java/com/example/painting/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.painting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Drawing drawingView;
+    private ImageButton currPaint;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawingView = findViewById(R.id.drawing);
+        LinearLayout paintLayout = findViewById(R.id.paint_colors);
+
+        currPaint = (ImageButton) paintLayout.getChildAt(0);
+        currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+    }
+
+    public void paintClicked(View view){
+        if(view != currPaint){
+            ImageButton imgView = (ImageButton) view;
+            String color = view.getTag().toString();
+
+            drawingView.setColor(color);
+
+            imgView.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+            currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint));
+            currPaint = (ImageButton) view;
+        }
+    }
+}
